77131a6f2046090b28c7e6e1821a4063e33e23d1
Nikita Kartashov <snailandmail@gmail.com>
2014-12-13 20:50:51 +0300

    Added Closure hw1

---
+++ closure1/src/core.clj
@@ -0,0 +1,33 @@
+(ns closure1.core
+ (:gen-class))
+
+(defn fun1
+ [f x]
+ (f x)
+ (f x))
+
+(defn fun2
+ [path]
+ (let [text (slurp path)]
+  (println text)
+  text)
+ )
+
+(def cube-anonymous (fn [x] (* x x x)))
+
+(defn fun4
+ [seq1 seq2]
+ ((comp reverse concat) seq1 seq2))
+
+(defn fun5
+ [elem input_seq]
+ (some (fn [x] (== x elem)) input_seq))
+
+(defn fun6
+ [seq1 seq2]
+ (let [zipped (map vector seq1 seq2)]
+  (remove (fn [x] (apply == x)) zipped)))
+
+(defn fun7
+ [elem n]
+ (map (fn [_] elem) (range n)))
