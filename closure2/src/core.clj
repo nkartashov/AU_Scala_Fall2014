69d455b040c2ad3c3434dce9d23b4a3bd891da6b
Nikita Kartashov <snailandmail@gmail.com>
2014-12-14 16:20:12 +0300

    Debuged my-fn macro

--- closure2/src/core.clj
+++ closure2/src/core.clj
@@ -10,4 +10,34 @@
     )
   )
 
+(defn split-names-values
+  [bindings]
+  (cons (take-nth 2 bindings)
+        (cons (take-nth 2 (rest bindings)) nil))
+  )
 
+(defmacro split-names-values-macro
+  [input]
+  (split-names-values input))
+
+(defmacro my-fn
+  [vars & body]
+  `(fn ~vars ~@body))
+
+
+(println (macroexpand '(my-fn [a b] (println a) (println b))))
+
+(def x (my-fn [a] (println a)))
+(x 10)
+
+;((my-fn [a] ((println a) (println 100))) 10)
+;(println (my-fn [a] ((println a) (println 100))))
+
+
+
+;(defmacro my-let
+;  [bindings & body]
+;  `((fn
+;      (vec (first (split-names-values-macro ~bindings)))
+;      (do ~@body)) (second (split-names-values-macro ~bindings)))
+;  )
