034cea3a0b83d9de9fe86e6fba454007b94723a0
Nikita Kartashov <snailandmail@gmail.com>
2014-12-14 19:38:57 +0300

    Fixed formatting

--- closure2/src/macros.clj
+++ closure2/src/macros.clj
@@ -1,20 +1,20 @@
- (ns macros)
+(ns macros)
 
- (defmacro my-or
+(defmacro my-or
   ([] nil)
   ([x] x)
   ([x & xs]
-   `(if ~x
-     true
-     (my-or ~@xs))))
+    `(if ~x
+       true
+       (my-or ~@xs))))
 
- (defmacro my-fn
+(defmacro my-fn
   [vars & body]
   `(fn ~vars ~@body))
 
- (defmacro my-let
+(defmacro my-let
   [bindings & body]
   `((my-fn ~(vec (take-nth 2 bindings)) ~@body) ~@(take-nth 2 (rest bindings))))
 
- (my-let [a 2] (println a))
- (my-let [c 3 a 2 b 3] (println (+ a b c)) (println (* b c a)))
+(my-let [a 2] (println a))
+(my-let [c 3 a 2 b 3] (println (+ a b c)) (println (* b c a)))
