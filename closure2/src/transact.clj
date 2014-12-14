02c1e920490fa21854e4587b8024d23e3e8164e9
Nikita Kartashov <snailandmail@gmail.com>
2014-12-14 18:55:48 +0300

    Added transactions

--- closure2/src/transact.clj
+++ closure2/src/transact.clj
@@ -1,1 +1,20 @@
- (ns transact)
+(ns transact)
+
+(def first-account (ref 0))
+(def second-account (ref 100))
+
+(defn print-accounts
+  []
+  (print "First account: " @first-account "\n")
+  (print "Second account: " @second-account "\n"))
+
+(print-accounts)
+
+(defn transfer [amount from to]
+  (dosync
+    (alter from - amount)
+    (alter to + amount)))
+
+(transfer 100 second-account first-account)
+
+(print-accounts)
